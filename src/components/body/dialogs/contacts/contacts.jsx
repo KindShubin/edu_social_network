113abe40f0da8d7009bdd9e4407c8fb9be1c5fa2
Anoshyn Dmitriy <anoshindmitriy@gmail.com>
2024-03-27 21:49:10 +0200

    refactoring Contacts. Lessons after BLL

--- src/components/body/dialogs/contacts/contacts.jsx
+++ src/components/body/dialogs/contacts/contacts.jsx
@@ -11,15 +11,15 @@ const val =
 //props.user
 
 const Contacts = () => {
-  const arrtest = Array.from(Object.entries(users));
-  const arrtestmap = arrtest.map((item) => 
+  const arrTestData = Array.from(Object.entries(users));
+  const arrtestmap = arrTestData.map((item) => 
     <NavLink to={`${item[0]}`} key={item[0]}> 
       <Contact user={item[1]} /> 
       </NavLink >);
 
 return (
   <div className={s.list}>
-    {arrtest.map(item => 
+    {arrTestData.map(item => 
     <NavLink to={`${item[0]}`} key={item[0]}> 
     <Contact user={item[1]} /> 
     </NavLink >)
